bd26dbf8ea70d8335781c3beb981636f45ba1e1a
Sophie Nagle <68610894+sophnagle@users.noreply.github.com>
2022-04-23 14:19:52 +0100

    Added images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import React from "react";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
 
-export default function App() {
+const images = [cabbage, mango, fig, gaze, peach, avocado];
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fielding
+        </h2>
+      </div>
+
+      <div className="image-container">
+        <img alt="" src="{images[0]}" />
+      </div>
     </div>
   );
-}
+};
+
+export default App;
